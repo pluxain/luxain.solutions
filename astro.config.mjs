94c19e69ab366c95fa13ad6cbe3bfd0a96578d9f
Pascal Luxain <pascal.luxain@gmail.com>
2024-10-01 09:44:19 +0200

    5: (setup) remove mandatory trailing slash in URLs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  trailingSlash: "always",
   i18n: {
     defaultLocale: "en",
     locales: ["en", "fr"],
