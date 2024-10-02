71365aa50b0293aaa94b2fc163733c9d6e77bf8a
Pascal Luxain <pascal.luxain@gmail.com>
2024-10-02 18:22:50 +0200

    5: (feat) define theme colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,34 @@
 export default {
   content: ["./src/**/*.{astro,html,ts,tsx}"],
   theme: {
+    backgroundColor: ({ theme }) => ({
+      accent: {
+        DEFAULT: theme("colors.accent.flip"),
+        flip: theme("colors.accent.DEFAULT"),
+      },
+      primary: {
+        DEFAULT: theme("colors.primary.flip"),
+        flip: theme("colors.primary.DEFAULT"),
+      },
+      success: {
+        DEFAULT: theme("colors.success.flip"),
+        flip: theme("colors.success.DEFAULT"),
+      },
+    }),
+    colors: {
+      accent: {
+        DEFAULT: "#4b5563",
+        flip: "#f9fafb",
+      },
+      primary: {
+        DEFAULT: "#3b82f6",
+        flip: "#eff6ff",
+      },
+      success: {
+        DEFAULT: "#22c55e",
+        flip: "#f0fdf4",
+      },
+    },
     extend: {},
   },
   plugins: [],
