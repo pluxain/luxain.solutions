5ef3e992f1ea2bf2bcd99d1a60b181fed33bf256
Pascal Luxain <pascal.luxain@gmail.com>
2024-10-04 15:48:09 +0200

    chore(setup): add scope enum values
    Ref: #19

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -2,6 +2,8 @@ export default {
   extends: ["@commitlint/config-conventional"],
   rules: {
     "references-empty": [2, "never"],
+    "scope-enum": [2, "always", ["ci", "content", "design", "setup"]],
+    "scope-case": [2, "always", "lower-case"],
     "type-enum": [2, "always", ["docs", "chore", "feat", "fix", "ref", "test"]],
   },
   parserPreset: {
