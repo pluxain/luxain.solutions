f7de7cc14bc36d138246c3b0aeb69fcce31d8f58
Pascal Luxain <pascal.luxain@gmail.com>
2024-10-04 16:49:26 +0200

    chore(setup): add tech as available scope for commit messages
    Ref: #21

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -2,7 +2,7 @@ export default {
   extends: ["@commitlint/config-conventional"],
   rules: {
     "references-empty": [2, "never"],
-    "scope-enum": [2, "always", ["ci", "content", "design", "setup"]],
+    "scope-enum": [2, "always", ["ci", "content", "design", "setup", "tech"]],
     "scope-case": [2, "always", "lower-case"],
     "type-enum": [2, "always", ["docs", "chore", "feat", "fix", "ref", "test"]],
   },
