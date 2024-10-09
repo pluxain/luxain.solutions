13a784164992d2e376031b9ac60c95d059dcf435
Pascal Luxain <pascal.luxain@gmail.com>
2024-10-09 15:38:27 +0200

    chore(setup): declare env variable types for transporter
    Ref: #21

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="../.astro/types.d.ts" />
+
+interface ImportMetaEnv {
+  readonly EMAIL_ACCOUNT: string;
+  readonly EMAIL_HOST: string;
+  readonly EMAIL_PASSWORD: string;
+  readonly EMAIL_PORT: number;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
