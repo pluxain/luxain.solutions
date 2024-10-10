450f8262d89c582b55e5777bb136879807ff2560
Pascal Luxain <pascal.luxain@gmail.com>
2024-10-10 15:27:14 +0200

    feat(tech): send email in action
    Ref: #21

--- src/actions/mail.ts
+++ src/actions/mail.ts
@@ -1,5 +1,7 @@
-import { defineAction } from "astro:actions";
+import { ActionError, defineAction } from "astro:actions";
 import { z } from "astro:schema";
+import { ConfigurationError } from "@libs/errors/ConfigurationError";
+import { sendMail } from "@libs/mail";
 
 export const mail = {
   send: defineAction({
@@ -9,8 +11,21 @@ export const mail = {
       message: z.string(),
       subject: z.string(),
     }),
-    handler: async ({ from, message, subject }) => {
-      return { from, message, subject };
+    handler: async (input) => {
+      try {
+        const response = await sendMail(input);
+        // TODO: log response
+        return { code: 250, message: response.response };
+      } catch (err) {
+        if (err instanceof ConfigurationError) {
+          throw new ActionError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "could not send the email",
+          });
+        }
+        // TODO: log error
+        throw err;
+      }
     },
   }),
 };
