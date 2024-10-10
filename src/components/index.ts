bee3892fff2f7e98728a178c8846ec04b494c715
Pascal Luxain <pascal.luxain@gmail.com>
2024-10-10 22:27:25 +0200

    ref(tech): make @components a package
    Ref: #21

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+import Footer from "./Footer.astro";
+import Header from "./Header.astro";
+import LanguageSwitcher from "./LanguageSwitcher.astro";
+import LanguageTag from "./LanguageTag.astro";
+import Link from "./Link.astro";
+import Notification from "./Notification.astro";
+
+export { Footer, Header, LanguageSwitcher, LanguageTag, Link, Notification };
